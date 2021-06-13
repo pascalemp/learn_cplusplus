20d93f97acec9be80364a23bd6e472ac9a99d414
pascalemp <michele.pascale111@gmail.com>
2021-06-13 21:52:23 +0100

    Added 0. hello_world

---
+++ 0. hello_world/hello_world.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "Hello World!";
+    
+    // std::getchar() allows keypress to allow a single char
+    // to be read from std#in; i.e. allows terminal
+    // to not exit prematurely.
+    std::getchar();
+    
+    return 0;
+
+}
